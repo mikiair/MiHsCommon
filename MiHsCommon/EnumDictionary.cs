6468272219caf04f234549bd8f061a9053ac77b2
Michael Heise <mh.mh@web.de>
2020-01-26 09:26:38 +0000

    introducing generic base EnumDictionary with method to check number of entries vs. number of keys in Enum StringExtensions: started refactoring of MatchPattern function (replace RegEx usage)
    git-svn-id: svn://192.168.1.10/MiHsCommon@4 71542346-207e-4e50-b83d-505b535b26f9

---
+++ MiHsCommon/EnumDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiHs.Common
+{
+    /// <summary>
+    /// Generic dictionary offering a method to check number of entries against number of values in TKey enum type
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class EnumDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        /// <summary>
+        /// Throws an ArgumentException when number of values in enum does not equal the number of dictionary entries
+        /// </summary>
+        /// <param name="enumType"></param>
+        protected void checkEntriesAfterInit()
+        {
+            if (Enum.GetValues(typeof(TKey)).Length != this.Count)
+            {
+                throw new ArgumentException("Invalid number of elements in dictionary " + this.GetType().Name + "!");
+            }
+        }
+    }
+}
--- MiHsCommon/StringExtensions.cs
+++ MiHsCommon/StringExtensions.cs
@@ -53,16 +53,49 @@ namespace MiHs.Common
         /// <summary>
         /// Converts a filename pattern to a regular expression
         /// </summary>
-        /// <remarks>Quoted from answer at https://stackoverflow.com/questions/652037/how-do-i-check-if-a-filename-matches-a-wildcard-pattern /// </remarks>
+        /// <remarks>Quoted from answer at https://stackoverflow.com/questions/652037/how-do-i-check-if-a-filename-matches-a-wildcard-pattern
+        /// </remarks>
         internal static class FindFilesPatternToRegex
         {
+            private static readonly char[] illegalChars = (@"\/:<>|" + "\"").ToCharArray();
+            //private static Regex IllegalCharactersRegex = new Regex("[" + @"\/:<>|" + "\"]", RegexOptions.Compiled);
+            private static Regex CatchExtensionRegex = new Regex(@"^\s*.+\.([^\.]+)\s*$", RegexOptions.Compiled);
             private static Regex HasQuestionMarkRegEx = new Regex(@"\?", RegexOptions.Compiled);
-            private static Regex IllegalCharactersRegex = new Regex("[" + @"\/:<>|" + "\"]", RegexOptions.Compiled);
-            private static Regex CatchExtentionRegex = new Regex(@"^\s*.+\.([^\.]+)\s*$", RegexOptions.Compiled);
 
             private static string NonDotCharacters = @"[^.]*";
 
             public static Regex Convert(string pattern)
+            {
+                pattern = checkPattern(pattern);
+
+                bool hasExtension = CatchExtensionRegex.IsMatch(pattern);
+
+                bool matchExact = false;
+                if (HasQuestionMarkRegEx.IsMatch(pattern))
+                {
+                    matchExact = true;
+                }
+                else if (hasExtension)
+                {
+                    matchExact = CatchExtensionRegex.Match(pattern).Groups[1].Length != 3;
+                }
+
+                string regexString = Regex.Escape(pattern);
+                regexString = "^" + Regex.Replace(regexString, @"\\\*", ".*");
+                regexString = Regex.Replace(regexString, @"\\\?", ".");
+
+                if (!matchExact && hasExtension)
+                {
+                    regexString += NonDotCharacters;
+                }
+
+                regexString += "$";
+
+                Regex regex = new Regex(regexString, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                return regex;
+            }
+
+            private static string checkPattern(string pattern)
             {
                 if (pattern == null)
                 {
@@ -73,30 +106,14 @@ namespace MiHs.Common
                 {
                     throw new ArgumentException("Pattern is empty.");
                 }
-                if (IllegalCharactersRegex.IsMatch(pattern))
+
+                if (pattern.IndexOfAny(illegalChars) >= 0)
+                //if (IllegalCharactersRegex.IsMatch(pattern))
                 {
                     throw new ArgumentException("Pattern contains illegal characters.");
                 }
-                bool hasExtension = CatchExtentionRegex.IsMatch(pattern);
-                bool matchExact = false;
-                if (HasQuestionMarkRegEx.IsMatch(pattern))
-                {
-                    matchExact = true;
-                }
-                else if (hasExtension)
-                {
-                    matchExact = CatchExtentionRegex.Match(pattern).Groups[1].Length != 3;
-                }
-                string regexString = Regex.Escape(pattern);
-                regexString = "^" + Regex.Replace(regexString, @"\\\*", ".*");
-                regexString = Regex.Replace(regexString, @"\\\?", ".");
-                if (!matchExact && hasExtension)
-                {
-                    regexString += NonDotCharacters;
-                }
-                regexString += "$";
-                Regex regex = new Regex(regexString, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                return regex;
+
+                return pattern;
             }
         }
     }
